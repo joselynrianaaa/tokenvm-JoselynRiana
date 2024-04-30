a491a4b949b6c775ef38936fc0ebf4ac4ca6d88d
Joselyn Riana <113001778+joselynrianaaa@users.noreply.github.com>
2024-05-01 00:12:54 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Joselyn's Hyperchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Joselyn"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JRM"
 )
 
 var ID ids.ID
